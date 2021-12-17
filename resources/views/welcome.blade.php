3a7cd4a6edd395fc3fc7d017e28a3f8a587cbc98
naoya.kato <xariarl84@gmail.com>
2021-12-17 03:28:10 +0000

    anime

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,19 +17,19 @@
         </div>
     @else    
         <div class="center junbotron">
-            <div class="text-center">
-                <h1 class="top">ようこそ！AnimalForumへ！<br>
-                AnimalForumは大切な家族であるペットの写真を共有する掲示板です！🐶🐱<br>
-                面白い写真や可愛い写真を投稿してみましょう！
-                </h1>
+            <div class="text-center mt-5 top">
+               <p class="h2 fade1 fadeUp">ようこそ！AnimalForumへ！</p>
+               <p class="h2 fade2 fadeUp">AnimalForumは大切な家族であるペットの写真を共有する掲示板です！🐶🐱</p>
+               <p class="h2 fade3 fadeUp">面白い写真や可愛い写真を投稿してみましょう！</p>
+               
             </div>
             <div class="d-flex flex-row justify-content-between mt-5">
-                <div class="col-md-4 offset-md-2"> {!! link_to_route('signup.get', '登録する！', [], ['class' => 'btn btn-lg btn-info ']) !!} </div>
-                <div class="col-md-4"> {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-info ']) !!} </div>
+                <div class="col-md-4 offset-md-2"> {!! link_to_route('signup.get', '登録する！', [], ['class' => 'btn btn-lg btn-info zoomOut']) !!} </div>
+                <div class="col-md-4"> {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-info zoomOut']) !!} </div>
             </div>
-            <div class="row justify-content-between">
-                <img src="{{ asset('images/dog2.jpg') }}" class="dog mt-5">
-                <img src="{{ asset("images/mark.jpeg") }}" class="dog1 mt-5">
+            <div class="row justify-content-around">
+                <img src="{{ asset('images/dog1.png') }}" class="dog mt-5 rounded-circle border border-dark zoomIn">
+                <img src="{{ asset("images/mark1.png") }}" class="dog mt-5 rounded-circle border border-dark zoomIn">
             </div>
         </div>
     @endif
