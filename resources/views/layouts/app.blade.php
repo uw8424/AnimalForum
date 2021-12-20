05fdcd67bfa659b372e36d8fb05a53b8c3371a74
naoya.kato <xariarl84@gmail.com>
2021-12-20 09:32:04 +0000

    anime

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
         <link rel="icon" href={{ asset("images/favicon.ico") }}>
-        <link rel="apple-touch-icon" href="{{ asset("images/apple-touch-icon") }}">
+        <link rel="apple-touch-icon" href="{{ asset("images/apple-touch-icon.png") }}">
     </head>
 
     <body>
